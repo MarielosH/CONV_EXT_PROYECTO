403484b1275eace67de4acec5324c522b9783539
MarielosH10 <marielos.herrera@qapybara.com>
2022-08-29 08:17:17 -0600

    Primeros Cambios BDD

---
+++ BDD/EPS 2022/RH_TIPO_AREA.sql
@@ -0,0 +1,14 @@
+SET DEFINE OFF;
+--SQL Statement which produced this data:
+--
+--  SELECT * FROM RRHH.RH_TIPO_AREA;
+--
+Insert into RRHH.RH_TIPO_AREA
+   (ID_AREA, NOMBRE_AREA)
+ Values
+   (1, 'ÁREA JURIDICCIONAL');
+Insert into RRHH.RH_TIPO_AREA
+   (ID_AREA, NOMBRE_AREA)
+ Values
+   (2, 'ÁREA ADMINISTRATIVA');
+COMMIT;
